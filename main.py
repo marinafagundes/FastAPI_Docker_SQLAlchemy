de2fa3698c7b3f8803a7d16ae336c065d78b44b2
Marina Fagundes e Souza <02020129566@intra.pgfn.gov.br>
2026-09-15 16:12:56 -0300

    Até a Aula 09 - Relação entre fornecedores e contas, precisa testar alembic e resolver imports

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import uvicorn
 from fastapi import FastAPI
 
 from contas_a_pagar_e_receber.routers import contas_a_pagar_e_receber_router
+from shared.exceptions import NotFound
+from shared.exceptions_handler import not_found_exception_handler
+
 # from shared.database import engine, Base
 
 # Informar qual modelo deve ser criado no banco de dados
@@ -24,6 +27,9 @@ def oi_eu_sou_programador():
 
 # Incluir rotas
 app.include_router(contas_a_pagar_e_receber_router.router)
+
+# Incluir Exception Handler
+app.add_exception_handler(NotFound, not_found_exception_handler)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
